ff64ed8c8d6d8a89cae4139de722c0dc2fcee732
connor-aylett <connor.aylett@duke.edu>
2022-07-18 11:53:29 -0400

    Improvements to Home Page

--- frontend/src/Components/HomePageInfoBox.js
+++ frontend/src/Components/HomePageInfoBox.js
@@ -1,14 +1,16 @@
-import styles from '../Styles/HomePageInfoBox.style.css'
+import styles from '../Styles/HomePage.style.css'
+import cupcake from '../Images/cupcake.jpeg';
 
-function HomePageInfoBox() {
+function HomePageInfoBox({picture, header, text}) {
     return (
         <div className='infoBox'>
             <div className='photoBox'>
-                <h1>Hello</h1>
+                <img src={picture}></img>
             </div>
             <div className='textBox'>
-                <h1>Hello2</h1>
-                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</p>
+                <h1>{header}</h1>
+                <br></br>
+                <p>{text}</p>
             </div>
         </div>
       );
--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,14 +1,17 @@
 import {useState, useEffect} from 'react';
 import cupcake from '../Images/cupcake.jpeg';
+import hbd from '../Images/hbd.jpg';
 import HomeStyled from '../Styles/Home.styled';
 import Navbar from '../Components/Navbar';
 import HomePageInfoBox from '../Components/HomePageInfoBox';
+import styles from '../Styles/HomePage.style.css'
+
 const Home = () => {
     return (
         <HomeStyled>
             <Navbar></Navbar>
             <h2 id="welcome">Welcome to Cumple Con Causa!</h2>
-            <div className="columns">
+            {/* <div className="columns">
                 <div className="container">
                     <img src={cupcake}></img>
                 </div>
@@ -16,8 +19,25 @@ const Home = () => {
                     <h2 id="caption">Work With an NGO to Make Your Birthday Special</h2>
                     <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</p>
                 </div>
+            </div> */}
+            <HomePageInfoBox picture={cupcake}
+            header="Our Vision & Mission"
+            text="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit."/>
+            <HomePageInfoBox picture={hbd}
+            header="History"
+            text="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit."/>
+            <br></br>
+            <div className='donationBox'>
+                <h1 className='donationText'>Donate to CCC! Buy a Hat!</h1>
+                <br></br>
+                <a href="https://google.com">
+                    <button className='donateButton'>Support CCC</button>
+                </a>
             </div>
-            <HomePageInfoBox></HomePageInfoBox>
+            <br></br>
+            <HomePageInfoBox picture={cupcake}
+            header="Impact & Reach"
+            text="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit."/>
         </HomeStyled>
     );
 }
