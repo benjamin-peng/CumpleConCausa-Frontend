3953b13c22b1a835eebf6edfa1e9f83e54ea0299
connor-aylett <connor.aylett@duke.edu>
2022-07-18 00:19:30 -0400

    Setting up flexboxes

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -2,6 +2,7 @@ import {useState, useEffect} from 'react';
 import cupcake from '../Images/cupcake.jpeg';
 import HomeStyled from '../Styles/Home.styled';
 import Navbar from '../Components/Navbar';
+import HomePageInfoBox from '../Components/HomePageInfoBox';
 const Home = () => {
     return (
         <HomeStyled>
@@ -16,6 +17,7 @@ const Home = () => {
                     <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</p>
                 </div>
             </div>
+            <HomePageInfoBox></HomePageInfoBox>
         </HomeStyled>
     );
 }
