c3a86012f660cac151af3e351902c5a14a0428c1
Benjamin Peng <benjamin.peng@duke.edu>
2022-07-20 23:13:09 -0400

    reposition bottom disclaimer

--- frontend/src/Styles/Bottom.styled.js
+++ frontend/src/Styles/Bottom.styled.js
@@ -4,11 +4,11 @@ export default styled.div`
     .bottom {
         border-top: 1px solid #f2f2f2;
         font-size: 12px;
-        position: fixed;
         bottom: 0;
         width: 100%;
         padding: 10px;
         background-color: white;
+        margin-top: auto;
     }
     .bottom p {
         color: rgb(203, 196, 196);
