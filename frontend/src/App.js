a25f6ad6423d0d1e051afc9098d17063609ccf4a
Benjamin Peng <benjamin.peng@duke.edu>
2022-07-30 17:26:43 -0700

    fix formatting on endpage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,29 +13,34 @@ import Endpage from './Pages/Endpage';
 import Partners from './Pages/Partners';
 import FAQs from './Pages/FAQs';
 
-var userData = {};
-const endpoint = 'http://localhost:3001/'; //replace w endpoint for server
-
-const setUserData = (d) => {
-  userData = d;
-  console.log(d);
-};
-
-const setCharity = (c) => {
-  userData.charity = c;
-};
-
-const postUserData = () => {
-  fetch(endpoint, {
-    method: 'POST',
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify(userData)
-  });
-};
-
 function App() {
 
   const [valid, setValid] = useState(false); //true only when user data form has been filled out
+  const [userData, setUserData] = useState({});
+
+  const endpoint = 'http://localhost:3001/'; //replace w endpoint for server
+  
+
+  /*
+  const setUserData = (d) => {
+    userData = d;
+    console.log(d);
+  };
+  */
+  
+  const setCharity = (c) => {
+    var copy = Object.assign({}, userData);
+    copy.charity = c;
+    setUserData(copy);
+  };
+  
+  const postUserData = () => {
+    fetch(endpoint, {
+      method: 'POST',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(userData)
+    });
+  };
 
   return (
     <Router>
@@ -48,10 +53,8 @@ function App() {
             <Route path="/register/video" element={<VideoOne />}></Route>
             <Route path="/register/form" element={<Form setUserData={setUserData} setValid={setValid}/>}></Route>
             <Route path="/register/questionnaire" element={<Questionnaire setCharity={setCharity} valid={valid} postUserData={postUserData}/>}></Route>
-            <Route path="/register/done" element={<Endpage />}></Route>
-
+            <Route path="/register/done" element={<Endpage userData={userData} />}></Route>
             <Route path="/FAQs" element={<FAQs />}></Route>
-
           </Routes>
         </div>
         <Bottom />
--- frontend/src/Pages/Endpage.js
+++ frontend/src/Pages/Endpage.js
@@ -1,9 +1,60 @@
-const Endpage = () => {
+import ngo_logo from "../Images/examplelogo.png";
+import left from "../Images/left_arrow.png";
+import right from "../Images/right_arrow.png";
+import { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
+import Navbar from '../Components/Navbar';
+import EndpageStyled from "../Styles/Endpage.styled";
+
+const Endpage = ({userData}) => {
+    
+    let ngos = userData.charity;
+
+    let max = userData.charity[0];
+    let second = userData.charity[0];
+
+    ngos.sort(function(a,b) {
+        return b.similarity - a.similarity
+    });
+
+    for (var i = 0 ; i < userData.charity.length; i++) {
+        if (max < userData.charity[i].similarity) {
+          max = userData.charity[i];
+        }
+    }
+
     return (  
-        <div className="endpage">
-            <h3>Thank you for your interest!</h3>
-            <p>An email will be sent with your details and a representative will contact you shortly to set up your Cumple Con Causa.</p>
-        </div>
+        <EndpageStyled>
+            <Navbar></Navbar>
+            <div className="endpage">
+                <h3>Awesome!</h3>
+                <p>Based on your answers to our questions, we think you might like working with the following NGOs:</p>
+                <div className="ngo">
+                    <h3 className="ngotitle">Your Top NGO</h3>
+                    <div className="circle"><img src={ngo_logo}>
+                        </img>
+                    </div>
+                    <b><p className="ngop">{ngos[0].name}</p></b>
+                    <p className="ngop">{ngos[0].line1} {ngos[0].line2} {ngos[0].line3}</p>
+                    <div className="arrows">
+                        <input type="image" src={left}/>
+                        <input type="image" src={right}/>
+                    </div>
+                    <i><p className="ngop">I will pick this NGO: </p></i>
+                    <select name="NGOs">
+                        <option value="Example 1">{ngos[0].name}</option>
+                        <option value="Example 2">{ngos[1].name}</option>
+                        <option value="Example 3">{ngos[2].name}</option>
+                    </select>
+                    <button className="ngobutton" type="button">CONFIRM</button>
+                </div>
+                <p>Not happy with your recommendations? Retake the quiz&nbsp;
+                    <a href="/register/form">here</a> or view a complete list of NGOs&nbsp;
+                    <a href="/list">here</a>.
+                </p>
+            </div>
+        </EndpageStyled>
+        
     );
 }
  
