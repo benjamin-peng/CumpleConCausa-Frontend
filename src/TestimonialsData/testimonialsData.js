db9040d907c3dbc52552f245b5dca725b2e75843
YouyouZzz <yzhao118@ucsc.edu>
2022-08-24 15:44:56 -0400

    Testimonial text update + page title changed
    need people's photo

--- src/TestimonialsData/testimonialsData.js
+++ src/TestimonialsData/testimonialsData.js
@@ -3,34 +3,61 @@ const people = [
     {
       id: 1,
       image: require('../Images/Person1.jpeg'),
-      name: "Person 1",
-      title: "Customer",
+      name: "Valeria Vazquez",
+    // title: "Customer",
       quote:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
+        "No sabrás el impacto positivo que causaste si no haces el intento de ayudar a los demás. Siempre quise hacer un evento dinámico para ayudar a una ONG; sin embargo, nunca lo intenté porque no sabía ni por dónde empezar. Al descubrir la cuenta de CCC, me motivé a pensar en un fundraiser y a mandar un DM para empezar. Al inicio, me propuse la idea de hacer un fundraiser divertido y puse una meta de B/.500. Después pensé que no lograría mi meta y que no recibiría tanto apoyo; un pensamiento que me desanimó mucho. Pero estuve MUY equivocada; al publicar el video que hice, mis amigos cercanos, no tan cercanos, familia y hasta personas que no conocía me inundaron de mensajes bonitos y apoyo. Definitivamente que fue una semana muy especial!! Algo que me conmovió muchísimo era una chica de Nueva Jersey que me mandó un mensaje porque quería un kit. Aunque al final se dio cuenta de lo complicado que era, igual se animó a donar lo que podía. Esto y muchos otros mensajes de apoyo alegraban mis días. Obviamente haría otro CCC, pero siento que lo único que cambiaría sería la dinámica; quizás recolectar donaciones o movilizar voluntarios, pero quiero seguir pensando en ideas creativas para ayudar a los demás. En un futuro, quisiera ver a más personas animándose a hacer el cambio y aportar su granito de arena y siento que CCC es un outlet perfecto para poder hacerlo! :) ",
     },
     {
       id: 2,
       image: require('../Images/Person2.png'),
-      name: "Person 2",
-      title: "Manager",
+      name: "Alexa Marie Flores Cruz",
+    //  title: "Manager",
       quote:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
+      "Me sorprendió la cantidad de personas que estaban dispuestas a sacar un tiempo dentro de su día para ir a comprar insumos y luego ofrecerse a traerlos al centro de recolección. También, me sorprendieron las donaciones de completos extraños que solo vieron la situación de necesidad y donaron. Cuando fui a entregar las donaciones, la chica de administración estaba muy confundida y me preguntaba '¿Pero cómo se llama tu fundación? ¿Con qué ONG vienes? ¿De parte de quién mandan todo esto?' y cuando le explique la iniciativa, se mostró inmensamente agradecida.",
     },
     {
       id: 3,
       image: require('../Images/Person1.jpeg'),
-      name: "Person 3",
-      title: "Volunteer",
+      name: "Eric Rodríguez",
+    //  title: "Volunteer",
       quote:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+      "Me sorprendió mucho cómo hay tantas personas que me apoyaron. Sí tenía una corazonada de que alcanzaría la meta, pero jamás pensé lograrlo en tan poco tiempo. Estoy muy feliz también porque independientemente de que pude recoger el dinero para la esterilización de 32 gatitos, pude darme cuenta de que aún existen personas que se preocupan por los demás y ayudan sin recibir nada a cambio",
     },
     {
       id: 4,
       image: require('../Images/Person2.png'),
-      name: "Person 4",
-      title: "Participant",
+      name: "Mary Gaby Bagatelas",
+    //  title: "Participant",
       quote:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+       "Me impacto mucho que la gente quiso donar más de lo que yo esperé. La gente se unió conmigo para cumplir un sueño tanto mío como de muchos niños que están en necesidad de aprender y de bajos recursos. Esto siento que nos unió como comunidad y lo haría de nuevo sin pensarlo dos veces",
+    },
+
+    {
+      id: 5,
+      image: require('../Images/Person1.jpeg'),
+      name: "Karla Castrellon",
+    //  title: "Volunteer",
+      quote:
+      "De los momentos más lindos fue cuando los niños mientras esperaban su desayuno me cantaran cumpleaños, se me salieron las lágrimas y solo agradecí a Dios por permitirme hacer este cumple con causa, sin duda me sentí muy muy agradecida con todo lo que me rodea, aun lo recuerdo y me conmuevo, solo gracias.",
+    },
+
+    {
+      id: 6,
+      image: require('../Images/Person1.jpeg'),
+      name: "Priscilla Lewis",
+    //  title: "Volunteer",
+      quote:
+      "Creo que todo se dió como se debia dar. Logramos y sobrepasamos la meta. Nos donaron personas que nisiquiera conocía. Le llegué a muchas personas que se sintieron identificadas y que me hicieron llegar sus sentimientos, creo que super exitoso y puedo decirte que me llenó el corazón de mucho amor, energía y felicidad. No hay nada que me guste mas que hacer feliz a otras personas y creo que lo logramos!",
+    },
+
+    {
+      id: 7,
+      image: require('../Images/Person1.jpeg'),
+      name: "María Paz Bejarano",
+    //  title: "Volunteer",
+      quote:
+      "ME ENCANTOOOO!!! Creo que lo que más me conmovió y me sorprendió fue uno de los diagnósticos que surgió en la gira. Era una señora mayor que nunca en su vida se había tomado la presión arterial, llevaba semanas con mucho dolor de cabeza y sentimiento de sofoco ocasional. La atendimos y nos dimos cuenta que estaba apunto de tener una crisis hipertensiva. Si no hubiéramos ido, es probable que la señora nunca se hubiera dado cuenta de su condición y puedo haber sucedido alguna tragedia. Afortunadamente pudimos recetarle los medicamentos adecuados y referirla para que le dieran seguimiento.",
     },
   ];
   
